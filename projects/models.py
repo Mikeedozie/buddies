98db31b08b81980393e5616a00983f8688fc04ed
Edozie <odigweedozie2@gmail.com>
2022-09-23 23:22:57 +0100

    added media settings to hold uploaded pictures, modified my gitignore file to ignore __pycache__ files, updated my models to include a many to many field in member model, referrring to area of speciality, so each member can have varieties of specialities to pick from

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 # Create your models here.
+#These are my models
+class SpecialityTags(models.Model):
+    SPECIALITY_CHOICES = (
+        ('GIT/GITHUB', 'Git/Github'),
+        ('SHELL', 'Shell'),
+        ('EMACS', 'Emacs'),
+        ('VI', 'Vi'),
+        ('C', 'C'),
+        ('PYTHON', 'Python'),
+
+    )
+    speciality = models.CharField(choices=SPECIALITY_CHOICES, max_length=100)
+
+    def __str__(self):
+        return self.speciality
+    class Meta:
+        verbose_name_plural = "Speciality Tags"
 
 class MemberActive(models.Manager):
     def active(self):
@@ -12,27 +29,20 @@ class Member(models.Model):
         ('Male', 'Male'),
         ('Female', 'Female')
     )
-    SPECIALITY_CHOICES = (
-        ('GIT/GITHUB', 'Git/Github'),
-        ('SHELL', 'Shell'),
-        ('EMACS', 'Emacs'),
-        ('VI', 'Vi'),
-        ('C', 'C'),
-        ('PYTHON', 'Python'),
-
-    )
+    
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     slug = models.SlugField()
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10)
-    speciality = models.CharField(choices=SPECIALITY_CHOICES, max_length=10)
+    speciality = models.ManyToManyField(SpecialityTags, blank=True)
     active = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     objects = MemberActive()
+    
+    def __str__(self):
+        return self.name
+
 class ProfileImage(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="profile_images")
     image = models.ImageField(upload_to="profile-images")
 
-class SpecialityTags(models.Model):
-    member = models.ManyToManyField(Member, blank=True)
-    name = models.CharField(max_length=100)
